edb9d647bbe312346fbcf9a8a547b066735f62e6
AndresITA23 <20151617@aguascalientes.tecnm.mx>
2023-12-02 12:24:02 -0600

    JS de Editar Perfil funcion guardar User

--- AutoFix Shop/js/editarPerfil.js
+++ AutoFix Shop/js/editarPerfil.js
@@ -18,6 +18,28 @@ document.addEventListener("DOMContentLoaded", function() {
     passwordUsuarioElement.value = currentUser.password;
 });
 
+document.getElementById('botonGuardar').addEventListener('click', function () {
+    guardarUser();
+});
+
+function guardarUser() {
+    // Obtén los valores de correo electrónico y contraseña y en este caso un nombre por default
+    var email = document.getElementById('correo').value;
+    var password = document.getElementById('password').value;
+    var nombre = document.getElementById('nombre').value;
+    // Valida los campos (puedes agregar más validaciones según tus requisitos)
+    if (email === '' || password === '' || nombre === '') {
+        alert('Por favor, completa todos los campos.');
+        return;
+    }
+    // Se establecen las credenciales del usuario (clase de usuario)
+    var user = new User(email, password, nombre);
+
+    localStorage.setItem('currentUser', JSON.stringify(user));
+
+    // Redirige a otra página o realiza otras acciones después de iniciar sesión
+    window.location.href = 'perfil.html';
+}
 
 
 class User {

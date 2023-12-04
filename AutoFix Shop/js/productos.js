615e15820c0c55f1a1e5ce61660257ad2d0aa9ff
aaaalleexx11 <20151606@aguascalientes.tecnm.mx>
2023-12-03 16:32:50 -0800

    productos JS data

---
+++ AutoFix Shop/js/productos.js
@@ -0,0 +1,82 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "AGP.1978 2 faros delanteros LED Par46 H5001 de 5.7 pulgadas",
+      descripcion: "Descripción del producto 1.",
+      categoria: "Faros",
+      precio: 1564.00,
+      imagen: "img/img5.png"
+    },
+    {
+        id: 2,
+        nombre: "Amortiguadores AG Compatible Para Chevrolet Tornado 2004-2010",
+        descripcion: "Descripción del producto 2.",
+        categoria: "Amortiguadores",
+        precio: 6500.00,
+        imagen: "img/img2.png"
+    },
+    {
+        id: 3,
+        nombre: "Aceite Motor Sintetico 5w20 Gf-5 Max Pro Tec 5 Litros Lth",
+        descripcion: "Descripción del producto 3.",
+        categoria: "Aceites",
+        precio: 989.00,
+        imagen: "img/img3.png"
+    },
+    {
+        id: 4,
+        nombre: "Moto Bateria LTH AGM CTX14-BS Premium",
+        descripcion: "Descripción del producto 4.",
+        categoria: "Baterías",
+        precio: 2530.00,
+        imagen: "img/img4.png"
+    },
+    {
+        id: 5,
+        nombre: "Power Stop KOE2316 - Kit de freno delantero y pastillas de freno de cerámica",
+        descripcion: "Descripción del producto 5.",
+        categoria: "Frenos",
+        precio: 6500.00,
+        imagen: "img/img1.png"
+    },
+    {
+        id: 6,
+        nombre: "Bosch 9697 - Bujía",
+        descripcion: "Descripción del producto 6.",
+        categoria: "Bujías",
+        precio: 916.00,
+        imagen: "img/img6.png"
+    },
+    {
+        id: 7,
+        nombre: "Auto Parasoles de Coche para Ventanas Frontales - Reflector UV Bloque (1 Pcs)",
+        descripcion: "Descripción del producto 7.",
+        categoria: "Parasoles",
+        precio: 110.00,
+        imagen: "img/img7.png"
+    },
+    {
+        id: 8,
+        nombre: "DEPO - Espejo retrovisor eléctrico con luz de señal de giro",
+        descripcion: "Descripción del producto 8.",
+        categoria: "Retrovisores",
+        precio: 1308.00,
+        imagen: "img/img8.png"
+    },
+    {
+        id: 9,
+        nombre: "Llanta Continental PremiumContact 2 195/65R15",
+        descripcion: "Descripción del producto 9.",
+        categoria: "Llantas",
+        precio: 2249.00,
+        imagen: "img/img9.png"
+    },
+    {
+        id: 10,
+        nombre: "Hoja Limpiaparabrisas Duralast",
+        descripcion: "Descripción del producto 10.",
+        categoria: "parabrisas",
+        precio: 189.00,
+        imagen: "img/img10.png"
+    },
+];

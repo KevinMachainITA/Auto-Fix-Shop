7dfa859a38673825195480c849ccabf714af372b
hugoperezNETICS <20151687@aguascalientes.tecnm.mx>
2023-12-03 19:53:17 -0600

    Archvio JSON de comentarios de productos

---
+++ AutoFix Shop/js/comentarios.js
@@ -0,0 +1,20 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Andrés Pérez Collazo",
+        descripcion: "Gran producto me ha funcionado y además es muy fácil de implementar",
+        estrellas: 4
+    },
+    {
+        id: 2,
+        nombre: "Luis Alfonoso Hernández Vargas",
+        descripcion: "Me ha encantado, la calidad del producto es bastante buena",
+        estrellas: 5
+    },
+    {
+        id: 3,
+        nombre: "Kevin Giovanni García Machain",
+        descripcion: "Muy buen producto, en perfectas condiciones, el único detalle es que tardó un poco en llegar",
+        estrellas: 4
+    },
+];
